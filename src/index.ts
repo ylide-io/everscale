465c2844b226e6bc050e28f71c4863f8291e0138
Danila Simonov <ek@ek-mail.ru>
2023-08-23 00:41:10 +0200

    connector scope + bump sdk & deps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import { ConnectorScope } from '@ylide/sdk';
+import {
+	everscaleBlockchainFactory,
+	everscaleWalletFactory,
+	venomBlockchainFactory,
+	venomWalletFactory,
+} from './controllers';
+
 export * from './contract-wrappers';
 export * from './controllers';
 export * from './encrypt';
 export * from './messages-sources';
 export * from './misc';
 export * from './network';
+
+export const tvm: ConnectorScope = {
+	walletFactories: [everscaleWalletFactory, venomWalletFactory],
+	blockchainFactories: [everscaleBlockchainFactory, venomBlockchainFactory],
+};
